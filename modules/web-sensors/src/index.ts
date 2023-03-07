5f55e035ff91d8d0c517f0c10659c38a295b1732
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-03-07 18:23:52 +0100

    fix: correctly export sensor source node

--- modules/web-sensors/src/index.ts
+++ modules/web-sensors/src/index.ts
@@ -1,4 +1,5 @@
 import { SensorSourceNode } from './web/SensorSourceNode';
 import { SensorSourceNode as SensorSourceNodeLegacy } from './legacy/SensorSourceNode';
 
+export { SensorSourceNode };
 module.exports = 'Accelerometer' in window ? { SensorSourceNode } : { SensorSourceNode: SensorSourceNodeLegacy };
